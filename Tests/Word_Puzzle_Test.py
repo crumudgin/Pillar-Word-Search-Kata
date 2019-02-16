122ad30d77561466492b88e0947c7fe5782745fe
Zac Chu <zac.chu@gmail.com>
2019-02-16 13:22:57 -0500

    Constructor now works for 2d puzzles

--- Tests/Word_Puzzle_Test.py
+++ Tests/Word_Puzzle_Test.py
@@ -6,7 +6,8 @@ from puzzle import Puzzle
 @pytest.mark.parametrize((	"file_name",			"rows",	"cols"),
 						[(	"point_puzzle.txt",		1,		1),	 # test that constructor works for simplest puzzle
 						(	"1D_puzzle.txt",		1,		5),  # test that constructor works with mutliple columns
-						(	"1D_puzzle_rows.txt",	5,		1)   # test that constructor works with multiple rows
+						(	"1D_puzzle_rows.txt",	5,		1),  # test that constructor works with multiple rows
+						(	"2D_puzzle.txt",		3,		3)
 						])
 def test_puzzle_constructor(file_name, rows, cols):
 	file_location = os.path.dirname(os.path.abspath(__file__)) + "/test_Puzzles/" + file_name
