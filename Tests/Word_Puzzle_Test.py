ea848034fc87f0c7560fb8cb814a5b5592fd1fcd
Zac Chu <zac.chu@gmail.com>
2019-02-16 13:21:28 -0500

    Puzzle constructor now works with multiple rows

--- Tests/Word_Puzzle_Test.py
+++ Tests/Word_Puzzle_Test.py
@@ -3,9 +3,10 @@ import os
 from puzzle import Puzzle
 
 
-@pytest.mark.parametrize((	"file_name",		"rows",	"cols"),
-						[(	"point_puzzle.txt",	1,		1),
-						(	"1D_puzzle.txt",	1,		5)
+@pytest.mark.parametrize((	"file_name",			"rows",	"cols"),
+						[(	"point_puzzle.txt",		1,		1),	 # test that constructor works for simplest puzzle
+						(	"1D_puzzle.txt",		1,		5),  # test that constructor works with mutliple columns
+						(	"1D_puzzle_rows.txt",	5,		1)   # test that constructor works with multiple rows
 						])
 def test_puzzle_constructor(file_name, rows, cols):
 	file_location = os.path.dirname(os.path.abspath(__file__)) + "/test_Puzzles/" + file_name
--- puzzle.py
+++ puzzle.py
@@ -2,5 +2,5 @@ class Puzzle():
 
 	def __init__(self, puzzle):
 		self.puzzle = puzzle.replace(", ", "").split("\n")
-		self.rows = 1
+		self.rows = len(self.puzzle)
 		self.cols = len(self.puzzle[0])
