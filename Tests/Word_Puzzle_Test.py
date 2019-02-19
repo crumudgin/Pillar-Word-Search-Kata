532236e4c9d4ca02e492a5deb16752f9e18cc2dd
Zac Chu <zac.chu@gmail.com>
2019-02-19 10:24:32 -0500

    corrected some spelling and added more tests that already work but should be formilized

--- Tests/Word_Puzzle_Test.py
+++ Tests/Word_Puzzle_Test.py
@@ -25,13 +25,13 @@ def test_puzzle_constructor(file_name, rows, cols, expected_puzzle):
 
 
 @pytest.mark.parametrize(("file_name",          "word", "location"),
-                        [("point_puzzle.txt",   "A",    [(0, 0)]),                   # test that the function can return the coordanites of a substring
+                        [("point_puzzle.txt",   "A",    [(0, 0)]),                   # test that the function can return the coordinates of a substring
                          ("1D_puzzle.txt",      "B",    [(0, 1)]),                   # test that the function works with multiple substrings
                          ("1D_puzzle.txt",      "AB",   [(0, 0), (0, 1)]),           # test that the function works with larger substrings
                          ("2D_puzzle.txt",      "E",    [(1, 1)]),                   # test that the function works with 2d puzzles
                          ("2D_puzzle.txt",      "GHI",  [(2, 0), (2, 1), (2, 2)]),   # test that the function works with larger substrings with 2d puzzles
-                         ("2D_puzzle.txt",      "AC",   []),                         # test that the function wnorks with substrings that are not in the puzzle
-                         ("2D_puzzle.txt",      "BA",   [(0, 1), (0, 0)])            # test that the function wnorks with substrings that are not in the puzzle
+                         ("2D_puzzle.txt",      "AC",   []),                         # test that the function works with substrings that are not in the puzzle
+                         ("2D_puzzle.txt",      "BA",   [(0, 1), (0, 0)])            # test that the function works with substrings that are not in the puzzle
                         ])
 def test_find_word_horizontal(file_name, word, location):
     puzzle = Puzzle(read_file(file_name))
@@ -39,13 +39,13 @@ def test_find_word_horizontal(file_name, word, location):
 
 
 @pytest.mark.parametrize(("file_name",          "word", "location"),
-                        [("point_puzzle.txt",   "A",    [(0, 0)]),                   # test that the function can return the coordanites of a substring
+                        [("point_puzzle.txt",   "A",    [(0, 0)]),                   # test that the function can return the coordinates of a substring
                          ("1D_puzzle_rows.txt", "B",    [(1, 0)]),                   # test that the function works with multiple substrings
                          ("1D_puzzle_rows.txt", "AB",   [(0, 0), (1, 0)]),           # test that the function works with larger substrings
                          ("2D_puzzle.txt",      "E",    [(1, 1)]),                   # test that the function works with 2d puzzles
                          ("2D_puzzle.txt",      "CFI",  [(0, 2), (1, 2), (2, 2)]),   # test that the function works with larger substrings with 2d puzzles
-                         ("2D_puzzle.txt",      "AC",   []),                         # test that the function wnorks with substrings that are not in the puzzle
-                         ("2D_puzzle.txt",      "DA",   [(1, 0), (0, 0)])            # test that the function wnorks with substrings that are not in the puzzle
+                         ("2D_puzzle.txt",      "AC",   []),                         # test that the function works with substrings that are not in the puzzle
+                         ("2D_puzzle.txt",      "DA",   [(1, 0), (0, 0)])            # test that the function works with substrings that are not in the puzzle
                         ])
 def test_find_word_vertical(file_name, word, location):
     puzzle = Puzzle(read_file(file_name))
@@ -53,48 +53,52 @@ def test_find_word_vertical(file_name, word, location):
 
 
 @pytest.mark.parametrize(("file_name",          "word", "location"),
-                        [("point_puzzle.txt",   "A",    [(0, 0)]),          # test that the function can return the coordanites of a substring
+                        [("point_puzzle.txt",   "A",    [(0, 0)]),          # test that the function can return the coordinates of a substring
                          ("2D_puzzle.txt",      "E",    [(1, 1)]),          # test that the function works with multiple substrings
                          ("2D_puzzle.txt",      "AE",   [(0, 0), (1, 1)]),  # test that the function works with multichar substrings
                          ("2D_puzzle.txt",      "C",    [(0, 2)]),          # test that the function works outside the matrices diagonal
                          ("2D_puzzle.txt",      "G",    [(2, 0)]),          # test that the function works outside the matrices diagonal
                          ("uneven_puzzle.txt",  "C",    [(0, 2)]),          # test that the function works with other matrix shapes
                         ])
-def test_find_word_diagnal_desending(file_name, word, location):
+def test_find_word_diagonal_descending(file_name, word, location):
     puzzle = Puzzle(read_file(file_name))
-    assert puzzle.find_word_diagnal_desending(word) == location
+    assert puzzle.find_word_diagonal_descending(word) == location
 
 
 @pytest.mark.parametrize(("file_name",              "word", "location"),
-                        [("point_puzzle.txt",       "A",    [(0, 0)]),          # test that the function can return the coordanites of a substring
+                        [("point_puzzle.txt",       "A",    [(0, 0)]),          # test that the function can return the coordinates of a substring
                          ("2D_puzzle.txt",          "E",    [(1, 1)]),          # test that the function works with multiple substrings
                          ("2D_puzzle.txt",          "CE",   [(0, 2), (1, 1)]),  # test that the function works with multichar substrings
                          ("2D_puzzle.txt",          "I",    [(2, 2)]),          # test that the function works outside the matrices diagonal
                          ("2D_puzzle.txt",          "A",    [(0, 0)]),          # test that the function works outside the matrices diagonal
                          ("uneven_puzzle.txt",      "F",    [(1, 2)]),          # test that the function works with other matrix shapes
                         ])
-def test_find_word_diagnal_assending(file_name, word, location):
+def test_find_word_diagonal_ascending(file_name, word, location):
     puzzle = Puzzle(read_file(file_name))
-    assert puzzle.find_word_diagnal_assending(word) == location
+    assert puzzle.find_word_diagonal_ascending(word) == location
 
 @pytest.mark.parametrize(("file_name",              "word", "location"),
-                        [("2D_puzzle.txt",          "ABC",    [(0, 0), (0, 1), (0, 2)]),  # test that the function works horizontaly
-                         ("2D_puzzle.txt",          "DEF",    [(1, 0), (1, 1), (1, 2)]),  # test that the function works horizontaly
-                         ("2D_puzzle.txt",          "GHI",    [(2, 0), (2, 1), (2, 2)]),  # test that the function works horizontaly
+                        [("2D_puzzle.txt",          "ABC",    [(0, 0), (0, 1), (0, 2)]),  # test that the function works horizontally
+                         ("2D_puzzle.txt",          "DEF",    [(1, 0), (1, 1), (1, 2)]),  # test that the function works horizontally
+                         ("2D_puzzle.txt",          "GHI",    [(2, 0), (2, 1), (2, 2)]),  # test that the function works horizontally
                          ("2D_puzzle.txt",          "ADG",    [(0, 0), (1, 0), (2, 0)]),  # test that the function works vertically
                          ("2D_puzzle.txt",          "BEH",    [(0, 1), (1, 1), (2, 1)]),  # test that the function works vertically
                          ("2D_puzzle.txt",          "CFI",    [(0, 2), (1, 2), (2, 2)]),  # test that the function works vertically
-                         ("2D_puzzle.txt",          "HD",     [(2, 1), (1, 0)]),          # test that the function works diagnally densending
-                         ("2D_puzzle.txt",          "AEI",    [(0, 0), (1, 1), (2, 2)]),  # test that the function works diagnally densending
-                         ("2D_puzzle.txt",          "BF",     [(0, 1), (1, 2)]),          # test that the function works diagnally densending
-                         ("2D_puzzle.txt",          "DB",     [(1, 0), (0, 1)]),          # test that the function works diagnally assending
-                         ("2D_puzzle.txt",          "GEC",    [(2, 0), (1, 1), (0, 2)]),  # test that the function works diagnally assending
-                         ("2D_puzzle.txt",          "HF",     [(2, 1), (1, 2)]),          # test that the function works diagnally assending
-                         ("2D_puzzle.txt",          "E",      [(1, 1)]),                  # test that the function works diagnally densending
-                         ("uneven_puzzle.txt",      "DEF",    [(1, 0), (1, 1), (1, 2)]),  # test that the function works on non-square matricies
-                         ("uneven_puzzle.txt",      "BE",     [(0, 1), (1, 1)]),          # test that the function works on non-square matricies
-                         ("uneven_puzzle.txt",      "BF",     [(0, 1), (1, 2)]),          # test that the function works on non-square matricies
-                         ("uneven_puzzle.txt",      "BD",     [(0, 1), (1, 0)]),          # test that the function works on non-square matricies
+                         ("2D_puzzle.txt",          "HD",     [(2, 1), (1, 0)]),          # test that the function works diagonally descending
+                         ("2D_puzzle.txt",          "AEI",    [(0, 0), (1, 1), (2, 2)]),  # test that the function works diagonally descending
+                         ("2D_puzzle.txt",          "BF",     [(0, 1), (1, 2)]),          # test that the function works diagonally descending
+                         ("2D_puzzle.txt",          "DB",     [(1, 0), (0, 1)]),          # test that the function works diagonally ascending
+                         ("2D_puzzle.txt",          "GEC",    [(2, 0), (1, 1), (0, 2)]),  # test that the function works diagonally ascending
+                         ("2D_puzzle.txt",          "HF",     [(2, 1), (1, 2)]),          # test that the function works diagonally ascending
+                         ("2D_puzzle.txt",          "E",      [(1, 1)]),                  # test that the function works diagonally descending
+                         ("uneven_puzzle.txt",      "DEF",    [(1, 0), (1, 1), (1, 2)]),  # test that the function works on non-square matrices
+                         ("uneven_puzzle.txt",      "BE",     [(0, 1), (1, 1)]),          # test that the function works on non-square matrices
+                         ("uneven_puzzle.txt",      "BF",     [(0, 1), (1, 2)]),          # test that the function works on non-square matrices
+                         ("uneven_puzzle.txt",      "BD",     [(0, 1), (1, 0)]),          # test that the function works on non-square matrices
+                         ("uneven_puzzle_2.txt",    "BDF",    [(0, 1), (1, 1), (2, 1)]),  # test that the function works on non-square matrices
+                         ("uneven_puzzle_2.txt",    "CD",     [(1, 0), (1, 1)]),          # test that the function works on non-square matrices
+                         ("uneven_puzzle_2.txt",    "CB",     [(1, 0), (0, 1)]),          # test that the function works on non-square matrices                    
+                         ("uneven_puzzle_2.txt",    "CF",     [(1, 0), (2, 1)]),          # test that the function works on non-square matrices                    
                         ])
 def test_find_word(file_name, word, location):
     puzzle = Puzzle(read_file(file_name))
--- puzzle.py
+++ puzzle.py
@@ -8,10 +8,10 @@ class Puzzle():
     def __find_word_in_matrix(self, word, strings, reversed_string=False):
         """
         Find the given word in the matrix using the provided strings.
-        Paramaters: word            - the substring the function is atempting to locate
+        Parameters: word            - the substring the function is attempting to locate
                     strings         - the strings the function should search
-                    reversed_string - the boolean that determins if the function should
-                                      attempt to find a solution with the string reveresd
+                    reversed_string - the boolean that determines if the function should
+                                      attempt to find a solution with the string reversed
         Returns: the coordinates of the word within the strings
         """
         for index, string in enumerate(strings):
@@ -26,12 +26,12 @@ class Puzzle():
 
     def __word_to_polar_coords(self, word, matrix):
         """
-        Logic for finding a substring in the diagnals of a matrix
-        Paramaters: word          - the substring the function is atempting to locate
+        Logic for finding the coordinates of a substring in the diagonals of a matrix
+        Parameters: word          - the substring the function is attempting to locate
                     matrix        - the matrix the function will search
                     coord_func    - a function containing the logic for
-                                      determaning the coordinates
-        Returns: the coordinates of the provided word within the desending diagnals
+                                      determining the coordinates
+        Returns: the coordinates of the provided word within the descending diagonals
                  of the matrix
         """
         string_lst = [[] for i in range(self.rows + self.cols - 1)]
@@ -43,15 +43,20 @@ class Puzzle():
 
         return self.__find_word_in_matrix(word, string_lst)
 
-    def __polar_coords_to_cartesian_coords(self, diagnal_coordinate, desending):
+    def __polar_coords_to_cartesian_coords(self, polar_coordinate, desending):
         """
-        BAD NEED TO REFACTOR
+        Logic for converting the polar coordinates to Cartesian coordinates
+        Parameters: polar_coordinate - a set of coordinates representing the 
+                                       index within the diagonal and index of the diagonal
+                    descending       - a boolean representing weather the polar coordinates
+                                       are based off of descending diagonals or ascending diagonals
+        Returns the Cartesian coordinates in the form (y, x)
         """
-        index, diagnal = diagnal_coordinate
+        index, diagonal = polar_coordinate
         col = 0 if desending else self.cols - 1
-        row = diagnal - self.cols + 1
-        if diagnal < self.cols:
-            col = (self.cols - diagnal - 1) if desending else diagnal
+        row = diagonal - self.cols + 1
+        if diagonal < self.cols:
+            col = (self.cols - diagonal - 1) if desending else diagonal
             row = 0
         col = (col + index) if desending else (col - index)
         row += index
@@ -60,8 +65,8 @@ class Puzzle():
     def __add_coordinates(self, current_coordinates, word, func):
         """
         Executes the provided func if the word has not been found yet.
-        Paramaters: current_coordinates - a list containing the coordinates of the word if found
-                    word                - the substring the function is atempting to locate
+        Parameters: current_coordinates - a list containing the coordinates of the word if found
+                    word                - the substring the function is attempting to locate
                     func                - the function to execute and return if the word has not
                                           been found
         Returns: The output of the provided func
@@ -72,21 +77,21 @@ class Puzzle():
 
     def find_word(self, word):
         """
-        Find an occurence of the given word if it exists within the puzzle matrix
-        Paramaters: word - the substring the function is atempting to locate
-        Returnes: the coordinaters of the word within the puzzle matrix
+        Find an occurrence of the given word if it exists within the puzzle matrix
+        Parameters: word - the substring the function is attempting to locate
+        Returns: the coordinates of the word within the puzzle matrix
         """
         coordinates = self.__add_coordinates([], word, self.find_word_horizontal)
         coordinates = self.__add_coordinates(coordinates, word, self.find_word_vertical)
-        coordinates = self.__add_coordinates(coordinates, word, self.find_word_diagnal_desending)
-        coordinates = self.__add_coordinates(coordinates, word, self.find_word_diagnal_assending)
+        coordinates = self.__add_coordinates(coordinates, word, self.find_word_diagonal_descending)
+        coordinates = self.__add_coordinates(coordinates, word, self.find_word_diagonal_ascending)
         return coordinates
 
     def find_word_horizontal(self, word):
         """
         Find the first horizontal instance of the provided word
-        Paramaters: word - the substring the function is atempting to locate
-        Returnes: the coordinates of the word if they exist
+        Parameters: word - the substring the function is attempting to locate
+        Returns: the coordinates of the word if they exist
         """
         coordinates = self.__find_word_in_matrix(word, self.matrix)
         return [(row, col) for col, row in coordinates]
@@ -94,8 +99,8 @@ class Puzzle():
     def find_word_vertical(self, word):
         """
         Find the first vertical instance of the provided word
-        Paramaters: word - the substring the function is atempting to locate
-        Returnes: the coordinates of the word if they exist
+        Parameters: word - the substring the function is attempting to locate
+        Returns: the coordinates of the word if they exist
         """
         string_lst = []
         for col in range(self.cols):
@@ -104,21 +109,21 @@ class Puzzle():
         coordinates = self.__find_word_in_matrix(word, string_lst)
         return [(row, col) for row, col in coordinates]
 
-    def find_word_diagnal_desending(self, word):
+    def find_word_diagonal_descending(self, word):
         """
-        Find the first diagnal desending instance of the provided word
-        Paramaters: word - the substring the function is atempting to locate
-        Returnes: the coordinates of the word if they exist
+        Find the first diagonal descending instance of the provided word
+        Parameters: word - the substring the function is attempting to locate
+        Returns: the coordinates of the word if they exist
         """
         reversed_matrix = [i[::-1] for i in self.matrix]
-        diagnal_coordinates = self.__word_to_polar_coords(word, reversed_matrix)
-        return [self.__polar_coords_to_cartesian_coords(i, True) for i in diagnal_coordinates]
+        polar_coordinates = self.__word_to_polar_coords(word, reversed_matrix)
+        return [self.__polar_coords_to_cartesian_coords(i, True) for i in polar_coordinates]
 
-    def find_word_diagnal_assending(self, word):
+    def find_word_diagonal_ascending(self, word):
         """
-        Find the first diagnal assending instance of the provided word
-        Paramaters: word - the substring the function is atempting to locate
-        Returnes: the coordinates of the word if they exist
+        Find the first diagonal assenting instance of the provided word
+        Parameters: word - the substring the function is attempting to locate
+        Returns: the coordinates of the word if they exist
         """
-        diagnal_coordinates = self.__word_to_polar_coords(word, self.matrix)
-        return [self.__polar_coords_to_cartesian_coords(i, False) for i in diagnal_coordinates]
+        polar_coordinates = self.__word_to_polar_coords(word, self.matrix)
+        return [self.__polar_coords_to_cartesian_coords(i, False) for i in polar_coordinates]
